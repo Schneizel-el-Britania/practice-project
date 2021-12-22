e19a2ecc944af9b6888d4f5fd26930a567f00358
Schneizel-el-Britania <kmmaxim1@gmail.com>
2021-12-22 17:30:09 +0200

    Add media requests for StartContest

--- client/src/components/HomeComponents/StartContest/index.js
+++ client/src/components/HomeComponents/StartContest/index.js
@@ -6,7 +6,7 @@ import styles from './StartContest.module.sass'
 export default function StartContest() {
   return (
     <article className={styles.container}>
-      <p>Ready to get started? Launch a contest and start receiving submissions instantly.</p>
+      <p className={styles.text}>Ready to get started? Launch a contest and start receiving submissions instantly.</p>
       <Link to={"/startContest"} className={styles.link}>
         <i className="far fa-lightbulb"></i>
         <span className={styles.linkText}>Start a contest</span>
