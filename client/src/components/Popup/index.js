3402be304e0ea0e492b7b91fc3683ecc294f1c83
Schneizel-el-Britania <kmmaxim1@gmail.com>
2022-01-15 16:40:10 +0200

    Add arrow for Popup

--- client/src/components/Popup/index.js
+++ client/src/components/Popup/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import cx from 'classnames';
 import styles from './Popup.module.sass';
 
-export default function Popup({content}) {
+export default function Popup({ content }) {
+  const arrowClasses = cx(styles.arrow, 'fas fa-chevron-left');
   return (
     <div className={styles.container}>
       <p>{content}</p>
+      <i className={arrowClasses} />
     </div>
   )
 }
